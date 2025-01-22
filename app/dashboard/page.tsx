e2e7a6928111dae5a1c2fa54451af7956a6f8de0
nahuelsosa2105 <nahuelsosa2105@gmail.com>
2025-01-22 01:21:34 +0000

    No se modifico nada, pero hubo problemas con git

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,13 @@ import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data';
 export default async function Page() {
     const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
-    const {numberOfInvoices, numberOfCustomers, totalPaidInvoices, totalPendingInvoices} = await fetchCardData();
+    const {
+      numberOfInvoices,
+      numberOfCustomers,
+      totalPaidInvoices,
+      totalPendingInvoices,
+    } = await fetchCardData();
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -30,3 +36,5 @@ export default async function Page() {
     </main>
   );
 }
+
+//hay cambios en alguna parte pero habria que vereficar si funciona bien
